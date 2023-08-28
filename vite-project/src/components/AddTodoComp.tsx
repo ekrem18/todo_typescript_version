53c55444340ac85a07a594d4a6a3e5c825bd42bd
Ekrem <avekremyilmazturk@gmail.com>
2023-08-28 21:27:06 +0300

    comps and pages

--- vite-project/src/components/AddTodoComp.tsx
+++ vite-project/src/components/AddTodoComp.tsx
@@ -1,9 +1,33 @@
+import { Box, Button, Container, TextField } from '@mui/material'
+import SaveIcon from "@mui/icons-material/Save";
 import React from 'react'
 
 const AddTodoComp = () => {
   return (
-    <div>AddTodoComp</div>
-  )
+    <Container>
+      <Box
+        sx={{
+          display: { xs: "block", sm: "flex" },
+          justifyContent: "center",
+          m: { xs: 4, sm: "auto" },
+          height: { xs: "120px", sm: "80px" },
+        }}>
+        <TextField
+          id="outlined-basic"
+          label="New Todo"
+          sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+          variant="outlined"
+          inputProps={{maxLength:40}}
+        />
+        <Button
+          variant="contained"
+          sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+          endIcon={<SaveIcon />}>
+          Save Todo
+        </Button>
+      </Box>
+    </Container>
+  );
 }
 
 export default AddTodoComp
--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -1,17 +1,47 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Typography } from "@mui/material";
+import AddTodoComp from "../components/AddTodoComp";
 interface TodoType {
   todo: string;
   isDone: boolean;
-  id: string | number ;
+  id: string | number; // id bilgisi string yada number olabilir. İki veri tipinide kabul edecek.
 }
 
 const Home = () => {
-  const[todos, setTodos] = useState<TodoType[]>([])
+  // const [todos, setTodos] = useState([] as TodoType[]);
+  // const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const url: string = import.meta.env.VITE_BASE_URL;
+
+  const getTodos = async () => {
+    try {
+      // const {data} = await axios(url)
+      const { data } = await axios<TodoType[]>(url);
+      console.log(data);
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
-    <div onClick={()=> setTodos([{id:5, todo:"emre", isDone:true }])}>Homes</div>
-  )
-}
+    <Container>
+      <Typography
+        color="error"
+        align="center"
+        variant="h2"
+        component={"h1"}
+        mt={3}>
+        Todo App with Typescript
+      </Typography>
+      <AddTodoComp/>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
