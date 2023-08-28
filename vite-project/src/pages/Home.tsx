1f4ba21cd48b4a867fd9c5ac83d9c19cadd20f3f
Ekrem <avekremyilmazturk@gmail.com>
2023-08-28 20:41:25 +0300

    home page

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+interface TodoType {
+  todo: string;
+  isDone: boolean;
+  id: string | number ;
+}
 
 const Home = () => {
+  const[todos, setTodos] = useState([])
+
   return (
     <div>Home</div>
   )
