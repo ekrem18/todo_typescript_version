2cb88906c747817aa875220eb95442d2151a335d
Ekrem <avekremyilmazturk@gmail.com>
2023-08-29 10:44:48 +0300

    last edits

--- vite-project/src/types.d.ts
+++ vite-project/src/types.d.ts
@@ -1,3 +1,16 @@
-// .d.ts uzantısnı typescript global alan olarak görüyor. Vu bu uzantıdaki dosyalarda tanımladığımız typelara herhangi bir export-import yapmadan erişim sağlayabiliyoruz
+// .d.ts uzantısnı typescript global alan olarak görüyor. dosyalarda tanımladığımız typelara herhangi bir export-import yapmadan erişim sağlayabiliyoruz
 
-type AddFn = (text: string) => Promise<void>;
+
+interface TodoType {
+    todo: string;
+    isDone: boolean;
+    id: string | number; // id bilgisi string yada number olabilir. İki veri tipi de kabul
+  }
+  
+  
+  type AddFn = (text: string) => Promise<void>;
+  
+  type ToggleFn = (todo:TodoType) => Promise<void>;
+  
+  type DeleteFn = (id: string | number) => Promise<void>;
+  
